c94383014cc77a1f24d51fa163a0ff082c0c76e9
Yury Benesh <ybxsoft@tut.by>
2014-11-24 21:28:21 +0300

    Added miod.Main

---
+++ parser_core/src/main/java/org/miod/Main.java
@@ -0,0 +1,9 @@
+package org.miod;
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Miod main!");
+        // TODO extract to a "compiler" subproject
+    }
+
+}
+
