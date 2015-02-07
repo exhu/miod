60a72adbf01427529c1a2afb6c8c60b3432c4e5a
Yury Benesh <ybxsoft@tut.by>
2015-02-07 11:08:27 +0300

    Reorganized build tree

--- parser_core/lexer_parser/src/main/java/org/miod/Main.java
+++
@@ -1,9 +0,0 @@
-package org.miod;
-public class Main {
-    public static void main(String [] args) {
-        System.out.println("Miod main!");
-        // TODO read source file, parse, dump parse tree
-    }
-
-}
-
