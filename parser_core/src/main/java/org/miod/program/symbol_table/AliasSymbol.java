001e8ea61908716c0f5b39a5d4d00f56209d9deb
Yury Benesh <ybxsoft@tut.by>
2016-07-11 23:54:23 +0300

    new symbol table items WIP

---
+++ parser_core/src/main/java/org/miod/program/symbol_table/AliasSymbol.java
@@ -0,0 +1,17 @@
+/*
+    Copyright 2016 Yury Benesh
+    see COPYING.txt
+ */
+package org.miod.program.symbol_table;
+
+/**
+ *
+ * @author yur
+ */
+public class AliasSymbol extends SymbolTableItem {
+
+    public AliasSymbol() {
+        super(null, null, null, null, null);
+    }
+
+}
