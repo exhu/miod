c175969c52a720934f25b808824a97eec8a50a70
Yury Benesh <ybxsoft@tut.by>
2016-07-16 21:39:30 +0300

    Defined types enum

---
+++ parser_core/src/main/java/org/miod/program/types/ValueTypeId.java
@@ -0,0 +1,33 @@
+/*
+    Copyright 2016 Yury Benesh
+    see COPYING.txt
+ */
+package org.miod.program.types;
+
+/** Expression and symbol table entry types.
+ *
+ * @author yur
+ */
+public enum ValueTypeId {
+    UNIT_DEF,
+    ALIAS,
+    TYPE_DEF,
+    INT,
+    FLOAT,
+    STRING_LITERAL,
+    NSTRING,
+    ARRAY_SIZED,
+    ARRAY_REF,
+    ENUM_DEF,
+    ENUM_VALUE,
+    STRUCT_DEF,
+    STRUCT_REF,
+    PROC_DEF,
+    PROC_REF,
+    CLASS_DEF,
+    CLASS_REF,
+    CLASS_WEAK,
+    CLASS_WEAK_REF,
+    METHOD_DEF,
+    METHOD_WITH_INSTANCE_REF,
+}
