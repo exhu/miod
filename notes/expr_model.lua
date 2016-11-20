6853e14c08e2bdfa43689e2a8ac8990457595989
Yury Benesh <ybxsoft@tut.by>
2016-11-20 22:41:02 +0300

    expr_model.lua

--- notes/expr_model.lua
+++ notes/expr_model.lua
@@ -18,10 +18,23 @@ end
 
 -- === ---
 
+-- constant folding phase, compile-time evaluation of expressions
 -- 'value' means known constant or literal
-M.node_kind = enum { 'runtime', 'value' }
+-- 'unknown' means undefined variable for 1st pass or error for 2nd pass
+M.node_value_kind = enum { 'unknown', 'runtime', 'value', 'generic' }
 
 pp(M.node_kind)
+
+function M.make_type_struct(type_name) 
+    -- value_ops = table of 'equals', 'greater', 'plus' etc. functions
+    return { name = type_name, value_ops = {} }
+end
+
+function M.make_node_result(kind, v_type)
+    assert(M.node_value_kind[kind])
+    return { value_kind = kind, value_type = v_type }
+end
+
 
 ----------------
 return M
