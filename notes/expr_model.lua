656a9250bf729690b07f3fc7d24db8d91f9c951f
Yury Benesh <ybxsoft@tut.by>
2016-11-26 20:24:11 +0300

    expr_model

--- notes/expr_model.lua
+++ notes/expr_model.lua
@@ -42,8 +42,63 @@ end
 --]]
 
 local sample_ast = { {'plus', nodes = { {value_kind = 'value', value = 3 },
-    {value_kind = 'value', value = 4}} }
+    {value_kind = 'value', value = 4}} } }
 
+local numeric_ops = enum { 'neg', 'bnot', 'mul', 'div', 'mod', 'plus', 'minus',
+    'bor', 'band', 'xor', 'shl', 'shr', 'gt', 'lt', 'ge', 'le', 'eq', 'ne' } 
+
+local function is_string(a,b)
+    return a.value_type == b.value_type and a.value_type == 'string'
+end
+
+local numeric_types = enum { 'cardinal', 'int32', 'uint32', 'int64', 'float', 'double' }
+
+local function is_numeric(a,b)
+    return numeric_types[a.value_type] and numeric_types[b.value_type]
+end
+
+local function calc_values(op, a,b)
+    if a.value_type == 'unknown' or b.value_type == 'unknown' then
+        return M.create_node_result('unknown', nil)
+    end
+
+    if is_string(a,b) then
+        if op == 'eq' or op == 'ne' or op == 'plus' then
+            if a.value_kind == 'value' and b.value_kind == 'value' then
+                if op == 'plus' then
+                    -- concatenate strings
+                    return M.create_node_result('value', 'string')
+                else
+                    return "unsupported operation"
+                end
+            end
+            return M.create_node_result('runtime', 'string')
+        end
+    else
+        if is_numeric(a,b) then
+            if numeric_ops[op] == nil then
+                return "unsupported operation"
+            end
+            if is_comparable(a,b) then
+                local new_type = promote(a,b)
+                local res = M.create_node_result('value', new_type)
+                -- do operation
+                res.value = "new value"
+                return res
+            else
+                return "incompatible numeric types"
+            end
+        else
+            if is_bool(a,b) then
+                -- todo validate op
+            else
+                return "unsupported operation for the types given"
+            end
+        end
+    end
+
+    return M.create_node_result('unknown', nil)
+end
 
 ----------------
 return M
