91c2c9eff87a8341d9bef6073acf14fb270d9366
Yury Benesh <ybxsoft@tut.by>
2016-12-03 15:56:26 +0300

    const assign

--- parser_core/src/main/java/org/miod/parser/visitors/SemanticVisitor.java
+++ parser_core/src/main/java/org/miod/parser/visitors/SemanticVisitor.java
@@ -4,6 +4,8 @@
  */
 package org.miod.parser.visitors;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.miod.parser.ParserContext;
 import static org.miod.parser.expr.ExpressionEval.exprEq;
 import static org.miod.parser.expr.ExpressionEval.exprLess;
@@ -14,6 +16,8 @@ import org.miod.parser.generated.MiodParserBaseVisitor;
 import org.miod.program.CompilationUnit;
 import org.miod.program.errors.BooleanExprExpected;
 import org.miod.program.errors.CompileTimeExpressionExpected;
+import org.miod.program.errors.SymbolRedefinitionError;
+import org.miod.program.symbol_table.SymbolTableItem;
 import org.miod.program.types.ValueTypeId;
 import org.miod.program.values.BoolValue;
 import org.miod.program.values.IntegerValue;
@@ -28,6 +32,7 @@ import org.miod.program.values.RuntimeValue;
  * @author yur
  */
 public class SemanticVisitor extends MiodParserBaseVisitor<MiodValue> {
+    private static final Logger LOGGER = Logger.getLogger(SemanticVisitor.class.getName());
 
     protected final ParserContext context;
     protected CompilationUnit unit;
@@ -36,6 +41,11 @@ public class SemanticVisitor extends MiodParserBaseVisitor<MiodValue> {
     public SemanticVisitor(String unitName, ParserContext ctx) {
         this.context = ctx;
         this.unitName = unitName;
+    }
+
+    /// signal error "expected expression" for second pass if v == null or ErrorValue
+    protected MiodValue checkValue(MiodValue v) {
+        return v;
     }
 
     @Override
@@ -88,41 +98,45 @@ public class SemanticVisitor extends MiodParserBaseVisitor<MiodValue> {
         return visit(ctx.literal());
     }
 
+
+
     @Override
     public MiodValue visitExprGreater(MiodParser.ExprGreaterContext ctx) {    ;
-        return invertBool(exprLessOrEqual(visit(ctx.left), visit(ctx.right),
-                context.getErrorListener()));
+        return checkValue(invertBool(exprLessOrEqual(visit(ctx.left), visit(ctx.right),
+                context.getErrorListener())));
     }
 
     @Override
     public MiodValue visitExprLess(MiodParser.ExprLessContext ctx) {        
-        return exprLess(visit(ctx.left), visit(ctx.right),
-                context.getErrorListener());
+        return checkValue(exprLess(visit(ctx.left), visit(ctx.right),
+                context.getErrorListener()));
     }
 
     @Override
     public MiodValue visitExprGreaterEq(MiodParser.ExprGreaterEqContext ctx) {        
-        return exprLessOrEqual(visit(ctx.right), visit(ctx.left),
-                context.getErrorListener());
+        return checkValue(exprLessOrEqual(visit(ctx.right), visit(ctx.left),
+                context.getErrorListener()));
     }
 
     @Override
     public MiodValue visitExprLessEq(MiodParser.ExprLessEqContext ctx) {        
-        return exprLessOrEqual(visit(ctx.left), visit(ctx.right),
-                context.getErrorListener());
+        return checkValue(exprLessOrEqual(visit(ctx.left), visit(ctx.right),
+                context.getErrorListener()));
     }
 
     @Override
     public MiodValue visitExprEquals(MiodParser.ExprEqualsContext ctx) {        
-        return exprEq(visit(ctx.left), visit(ctx.right),
-                context.getErrorListener());
+        return checkValue(exprEq(visit(ctx.left), visit(ctx.right),
+                context.getErrorListener()));
     }
 
     @Override
     public MiodValue visitExprNotEq(MiodParser.ExprNotEqContext ctx) {        
-        return invertBool(exprEq(visit(ctx.left), visit(ctx.right),
-                context.getErrorListener()));
+        return checkValue(invertBool(exprEq(visit(ctx.left), visit(ctx.right),
+                context.getErrorListener())));
     }
+
+
 
     // TODO special case for stuct recursion in definition, e.g.
     // 1) type mystruct = struct parent: mystruct end_struct --
@@ -130,4 +144,37 @@ public class SemanticVisitor extends MiodParserBaseVisitor<MiodValue> {
     // 2) type myclass = class parent: myclass end_class
     // -- class type is mutable, so it's put into parent SymbolTable and
     // filled as parsing goes further.
+
+    @Override
+    public MiodValue visitConstExpr(MiodParser.ConstExprContext ctx) {
+        return visit(ctx.expr()); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public MiodValue visitConstAssign(MiodParser.ConstAssignContext ctx) {
+        LOGGER.log(Level.INFO, "const assign var = {0}, expr = {1}", new String[] {
+                ctx.bareName().getText(),
+                ctx.constExpr().getText()});
+
+        // check if already defined
+        String id = ctx.bareName().getText();
+        SymbolTableItem sym = unit.symTable.resolve(id);
+        if (sym != null) {
+            context.getErrorListener().onError(new SymbolRedefinitionError(sym));
+            return null;
+        }
+
+        // TODO create symbol, put to table
+
+        if (ctx.typeSpec() != null) {
+            // TODO check type of the expr
+            LOGGER.log(Level.INFO, "typed const");
+        } else {
+            // TODO type of the expr
+            LOGGER.log(Level.INFO, "auto const");
+        }
+        return super.visitConstAssign(ctx);
+    }
+
+
 }
